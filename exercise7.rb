6e99b6144c19c79589378e273ad26e1e8a17cf9d
ryanmmhmm <magowan.ryan@gmail.com>
2015-10-28 15:58:21 -0400

    increase cohort size by 5% and redisplay

--- exercise7.rb
+++ exercise7.rb
@@ -21,5 +21,14 @@ printHash(students)
 # Step 2
 students[:cohort4] = 43
 
+
 # Step 3
 puts students.keys
+
+
+# Step 4
+students.each do | name, num_students |
+	students[name] = num_students * 1.05
+end
+
+printHash(students)
