b37d916c009bcab8fd48bbd0e4d386804caf1534
ryanmmhmm <magowan.ryan@gmail.com>
2015-10-28 14:44:13 -0400

    initial commit, completed exercise, method returns C value

---
+++ exercise5.rb
@@ -0,0 +1,7 @@
+## Exercise 5 W1D3
+
+def F_to_C(temp)
+	celcius = (temp.to_i - 32) * 5 / 9 # convert from F to C
+	puts "The temperature #{temp}F is equal to #{celcius}C."  # print output
+	return celcius	# have the method return the value in C
+end
