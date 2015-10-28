e03acb7e6d7fa0ee4a4ad7e9c26f529220b6f584
ryanmmhmm <magowan.ryan@gmail.com>
2015-10-28 14:54:32 -0400

    add rice to list and print list again with *

--- exercise6.rb
+++ exercise6.rb
@@ -1,8 +1,16 @@
 ## Exercise 6 -- W1D3
 
-grocery_list = ["carrots", "toilet paper", "apples", "salmon"]
-
-
-grocery_list.each do |item|
-	puts "* #{item}"
+def starList(list)
+	list.each do |item|
+		puts "* #{item}"
+	end
 end
+
+# Step 1
+grocery_list = ["carrots", "toilet paper", "apples", "salmon"]
+starList(grocery_list)
+
+
+# Step 2
+grocery_list << "rice"
+starList(grocery_list)
