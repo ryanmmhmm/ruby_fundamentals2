cabfd1680fd5ab1321d98b1e48a8464310c7f86b
ryanmmhmm <magowan.ryan@gmail.com>
2015-10-28 16:17:16 -0400

    students are integers, sumStudent method created, added some comments

--- exercise7.rb
+++ exercise7.rb
@@ -7,6 +7,19 @@ def printHash(in_hash)
 end
 
 
+# Step 6 (BONUS ROUND!)
+def sumStudents(in_hash)
+	sum = 0  # initialize sum
+
+	in_hash.each do | name, num_students |
+		sum = sum + num_students.to_i
+	end
+
+	puts "There are #{sum} students in all of the cohorts."
+	return sum
+end
+
+
 students = {
 	cohort1: 34,
 	cohort2: 42,
@@ -28,12 +41,16 @@ puts students.keys
 
 # Step 4
 students.each do | name, num_students |
-	students[name] = num_students * 1.05
+	students[name] = (num_students * 1.05).to_i  # students can't be fractional
 end
 
 printHash(students)
 
 
 # Step 5
-students.delete(:cohort2)
+students.delete!(:cohort2)
 printHash(students)
+
+
+# Step 6 (BONUS ROUND)
+sumStudents(students)
